df34f0bedccf18a3aac46e252612ac5141bebd3f
Julia050889 <xlxatyanjulia@gmail.com>
2022-09-23 21:11:34 +0400

    product_page

--- Desktop/quiz_app/lib/widgets/detail_page.dart
+++ Desktop/quiz_app/lib/widgets/detail_page.dart
@@ -1,39 +1,35 @@
- import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/widgets/bottom_bar.dart';
 
 Widget detailPage(product) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView(children: [
-          Container(
-              margin: const EdgeInsets.all(15),
-              height: 150,
-              child: CarouselSlider(
-                items: [
-                  Image.network(product.imageUrl),
-                  Image.network(product.imageUrl)
-                ],
-                options: CarouselOptions(),
-              )),
-          const Divider(),
-          Text(
-            product.productName,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Text(product.description),
-          const SizedBox(height: 180),
-        ]),
-      ),
-      bottomNavigationBar: bottomBar(product),
-    );
-  }
+  return Scaffold(
+    appBar: AppBar(),
+    body: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: ListView(children: [
+        Container(
+            margin: const EdgeInsets.all(15),
+            height: 150,
+            child: CarouselSlider(
+              items: [
+                Image.network(product.imageUrl),
+                Image.network(product.imageUrl)
+              ],
+              options: CarouselOptions(),
+            )),
+        const Divider(),
+        Text(
+          product.productName,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Text(product.description),
+        const SizedBox(height: 180),
+      ]),
+    ),
+    bottomNavigationBar: bottomBar(product),
+  );
+}
